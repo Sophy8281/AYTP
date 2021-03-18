fbedae81df21e89061a8677eb16bd51b9ad524c6
AmaniUsagi <newton.amani@strathmore.edu>
2021-03-18 18:08:06 +0300

    Project backbone commit 1
    Building the skeleton to enable routing, uploads and other basic functionalities between react and django

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+function App() {
+    return(
+        <div className="App h-screen">
+            <div className="w-full flex flex-wrap">
+                <div className="w-full md:w-1/2 flex flex-col">
+                    <div className="flex justify-center md:justify-start pt-12 md:pl-12 md:-mb-24">
+                        <p className="bg-black text-white font-bold text-xl p-4 rounded">Logo</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default App;
